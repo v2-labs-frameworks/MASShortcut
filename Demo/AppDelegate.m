d6c2f0fad9558b7f1118db9f7ab4235fa7f425f3
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2018-11-08 08:26:34 -0200

    Fix a reference in MASShortcut Demo Application
    This change provides a recommended self-> reference fix for the MASShortcut
    Demo application.
    It also enables the Base translation in Xcode.

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -53,7 +53,7 @@ static void *MASObservingContext = &MASObservingContext;
     [[NSSound soundNamed:@"Ping"] play];
     [_feedbackTextField setStringValue:NSLocalizedString(@"Shortcut pressed!", @"Feedback that’s displayed when user presses the sample shortcut.")];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [_feedbackTextField setStringValue:@""];
+        [self->_feedbackTextField setStringValue:@""];
     });
 }
 
